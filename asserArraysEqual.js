5ccfdba3f2e936a26a3219ec0b5389348ff68816
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-18 10:15:37 -0700

    completed asserArraaysEqual.js

---
+++ asserArraysEqual.js
@@ -0,0 +1,17 @@
+const assertArraysEqual = function (array1, array2) {
+  let array = true;
+
+  if (array1.length !== array2.length) {
+    array = false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] != array2[i]) {
+      array = false;
+    }
+  }
+  if (array) {
+    console.log("✔✔ Assertion Passed");
+  } else { 
+    console.log("❌ Assertion Failed");
+  }
+}
