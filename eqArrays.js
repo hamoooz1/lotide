a5aa738e9b60e4eb91fe50f7a7e40f3528824173
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-23 15:08:33 -0700

    refactored

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✔✔ Assertion Passed: ${actual} === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const { builtinModules } = require("module");
 
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
@@ -17,11 +11,5 @@ const eqArrays = function (array1, array2) {
   }
   return true;
 }
-// TEST CODE
-console.log(eqArrays([1, 2, 3], [1, 2])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
 
-eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
-eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+module.exports = eqArrays;
