a8324c97e46f5d3c49ed77e4f9d46a74a8c2d623
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-23 16:00:36 -0700

    refactored

--- middle.js
+++ middle.js
@@ -1,32 +1,5 @@
-const eqArrays = function (array1, array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertArraysEqual = function (array1, array2) {
-  let array = true;
-
-  if (array1.length !== array2.length) {
-    array = false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] != array2[i]) {
-      array = false;
-    }
-  }
-  if (array) {
-    console.log("✔✔ Assertion Passed");
-  } else { 
-    console.log("❌ Assertion Failed");
-  }
-}
+const eqArrays = require("./eqArrays");
+const assertArraysEqual = require("./assertArraysEqual");
 
 const middle = function (array) {
   let middles = [];
@@ -45,5 +18,4 @@ const middle = function (array) {
   return middles;
 }
 
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [2]
-console.log(middle([1, 2, 3, 4, 5]))
+module.exports = middle;
