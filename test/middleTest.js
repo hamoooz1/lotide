b5c1cc84d304ec439dc696e3ceda7eadf0b953e6
Hamza Kaderi <kaderihamza4@gmail.com>
2023-10-23 15:58:42 -0700

    refactored

---
+++ test/middleTest.js
@@ -0,0 +1,6 @@
+const middle = require("../middle");
+const assertArraysEqual = require("../assertArraysEqual");
+
+//Test Code
+console.log(middle([1, 2, 3, 4, 5, 6])) // => [2]
+console.log(middle([1, 2, 3, 4, 5]))
